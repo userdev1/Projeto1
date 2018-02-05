1656fc930824535896b61ea9f72acb64e622d99a
Daniel <wowmegadenis@hotmail.com>
2018-02-05 02:14:34 -0200

    Create restaurant model

---
+++ app/src/main/java/lustoza/learning1/api/Restaurant.java
@@ -0,0 +1,16 @@
+package lustoza.learning1.api;
+
+/**
+ * Created by bloder on 05/02/18.
+ */
+
+public class Restaurant {
+
+    public final String name;
+    public final String image;
+
+    public Restaurant(String name, String image) {
+        this.name = name;
+        this.image = image;
+    }
+}
